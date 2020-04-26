b14623e4b0e4bb4290fc60fba05c75b67e442962
dharmicksai <55967538+dharmicksai@users.noreply.github.com>
2020-04-26 22:23:14 +0530

    Edited Symetric encryptor-decryptor

--- symyetric_encrypt.py
+++ symyetric_encrypt.py
@@ -1,6 +1,3 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
